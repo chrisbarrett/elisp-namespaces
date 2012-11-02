2a1d736663af0fa4624efe35f533e2a008f2670c
Chris Barrett <chris.d.barrett@me.com>
2012-11-02 21:06:27 +1300

    passing var redefinition tests

--- namespaces-tests.el
+++ namespaces-tests.el
@@ -54,12 +54,22 @@
   (should-not (@sym undefined)))
 
 
-;;; def, @, @set
+;;; def
 
-(check "can define and read namespaced var"
+(check "can define and read var created with def"
   (def var 'expected)
   (should (equal 'expected (@ var))))
 
+(check "should get error when setting a var created with def"
+  (def var nil)
+  (should-error (eval `(@set var 'fail))))
+
+
+;;; defmutable
+
+(check "can define and read var created with defmutable"
+  (defmutable var 'expected)
+  (should (equal 'expected (@ var))))
 
 (check "can set namespaced var created with defmutable"
   (defmutable var)
@@ -67,13 +77,21 @@
   (should (equal 'expected (@ var))))
 
 
-(check "should get error when setting a var created with def"
-  (def var nil)
-  (should-error (eval `(@set var 'fail))))
-
+;;; Variable Definition
 
 (check "should get error when getting the value of an undefined var"
   (should-error (eval `(@ fail))))
+
+(check "can redefine def vars as defmutable vars and set"
+  (def var nil)
+  (defmutable var)
+  (@set var 'expected)
+  (should (equal 'expected (@ var))))
+
+(check "should get error when redefining a defmutable as a def and using @set"
+  (defmutable var)
+  (def var nil)
+  (should-error (eval `(@set var 'x))))
 
 
 ;;; defn, @call
