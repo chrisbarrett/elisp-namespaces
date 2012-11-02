4660c0008881304bc739d3543a96f34931b98dab
Chris Barrett <chris.d.barrett@me.com>
2012-11-03 01:48:37 +1300

    written tests for ns-core

---
+++ ns-core.el
@@ -0,0 +1,95 @@
+;; NS-CORE
+;;
+;; Copyright (c) 2012, Chris Barrett
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;
+;; 1. Redistributions of source code must retain the above copyright notice, this
+;;    list of conditions and the following disclaimer.
+;; 2. Redistributions in binary form must reproduce the above copyright notice,
+;;    this list of conditions and the following disclaimer in the documentation
+;;    and/or other materials provided with the distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+;; ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+;; ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+;; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+;; ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;
+;;
+;; DESCRIPTION:
+;; Core namespace functions.
+;;
+
+(require 'cl)
+
+(defstruct ns-meta
+  public?
+  mutable?)
+
+(defconst ns/symbols-table (make-hash-table :test 'equal)
+  "Maps tuples of (hash * sym) to symbol metadata.")
+
+(equal '(1 2) '(1 2))
+
+;;; --------------------------- Symbol Processing ---------------------------------
+
+(defun ns/hash (sym)
+  "Hash a symbol for insertion into lookup tables."
+  (let ((hash (secure-hash 'sha256 (symbol-name sym))))
+    (intern (concat "__ns/namespaced_sym__" hash))))
+
+(defun ns/split-sym (sym)
+  "Splits SYM into a tuple of (namespace * symbol)."
+  (let* ((xs (split-string (symbol-name sym) "/"))
+         (s1 (intern (car xs)))
+         (s2 (when (nth 1 xs) (intern (nth 1 xs)))))
+    (if (< 1 (length xs))
+        `(,s1 . ,s2)
+      `(,nil . ,s1))))
+
+(defun ns/qualify (ns sym)
+  "Ensure SYM is qualified with namespace NS."
+  (let* ((tpl  (__ns/split-sym sym))
+         (ns   (symbol-name ns))
+         (name (symbol-name (nth 1 tpl))))
+    (intern (concat ns "/" name))))
+
+
+(defun ns/make-key (ns sym)
+  "Make a key for the symbols table."
+  (let* ((sym  (ns/qualify ns sym))
+         (hash (ns/hash sym)))
+    `(,hash . ,sym)))
+
+;;; --------------------------- Table Accessors -----------------------------------
+
+(defun ns/intern (ns sym)
+  "Intern the given ns/sym into the namespace table."
+  (puthash (ns/make-key ns sym)
+           (make-ns-meta)
+           ns/symbols-table))
+
+(defun hash-keys (table)
+  (loop for k being the hash-keys of table
+        collect k))
+
+(defun ns/get-symbol-name (hash)
+  "Returns the interned symbol name corresponding to HASH, or nil"
+  (let ((filtered (remove-if-not (lambda (tpl) (equal hash (car tpl)))
+                                 (hash-keys ns/symbols-table))))
+    (cdr-safe (car-safe filtered))))
+
+(defun ns/get-symbol-hash (ns sym)
+  "Returns the interned symbol name corresponding to HASH, or nil"
+  (let* ((sym (ns/qualify ns sym))
+         (filtered (remove-if-not (lambda (tpl) (equal sym (cdr tpl)))
+                                  (hash-keys ns/symbols-table))))
+    (car-safe (car-safe filtered))))
---
+++ tests/ns-core-tests.el
@@ -0,0 +1,67 @@
+;; NS-CORE-TESTS
+
+(require 'ert)
+(require 'cl)
+
+(defmacro check (desc &rest body)
+  "Wrap ert-deftest with a simpler interface for testing namespaces."
+  (declare (indent 1))
+  `(ert-deftest
+       ,(intern (replace-regexp-in-string "[ .]" "-" desc)) ()
+     ;; Rebind tables for tests.
+     (let ((ns/symbols-table (copy-hash-table ns/symbols-table)))
+       ,@body)))
+
+
+;;; Hashing
+
+(check "hash function returns different hashes for different inputs."
+  (should-not (equal (ns/hash 'y) (ns/hash 'x))))
+
+(check "hash function same hash for identical inputs"
+  (should (equal (ns/hash 'x) (ns/hash 'x))))
+
+;;; Keygen
+
+(check "keys should have structural equality"
+  (let ((k1 (ns/make-key 'x 'y))
+        (k2 (ns/make-key 'x 'y)))
+    (should (equal k1 k2))))
+
+;;; Interning
+
+(check "can intern symbols for a given namespace"
+  (ns/intern 'foo 'bar)
+  (should (= 1 (hash-table-count ns/symbols-table))))
+
+(check "after several interns the same symbol is not present multiple times"
+  (ns/intern 'foo 'bar)
+  (ns/intern 'foo 'bar)
+  (should (= 1 (hash-table-count ns/symbols-table))))
+
+;;; Retrieval
+
+(check "can get name by hash for interned symbol"
+  (ns/intern 'foo 'bar)
+  (let* ((hash (ns/hash 'foo/bar)))
+    (should (equal 'foo/bar (ns/get-symbol-name hash)))))
+
+(check "can get hash by name for interned symbol"
+  (ns/intern 'foo 'bar)
+  (let* ((hash (ns/hash 'foo/bar)))
+    (should (equal hash (ns/get-symbol-hash 'foo 'bar)))))
+
+;;; Retrieving Uninterned Syms
+
+(check "get name by hash for uninterned symbol returns nil"
+  (should (equal nil (ns/get-symbol-name 'invalid))))
+
+(check "get hash by name for uninterned symbol returns nil"
+  (should (equal nil (ns/get-symbol-hash 'ns 'invalid))))
+
+
+
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
