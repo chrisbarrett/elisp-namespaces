f182dd0f6d66e01501dd31997e8324fe8538705f
Chris Barrett <chris.d.barrett@me.com>
2012-11-03 13:17:11 +1300

    Move check macro into test harness

--- tests/ns-core-tests.el
+++ tests/ns-core-tests.el
@@ -2,16 +2,6 @@
 (require 'ns-core)
 (require 'ert)
 
-(defmacro check (desc &rest body)
-  "Wrap ert-deftest with a simpler interface for testing namespaces."
-  (declare (indent 1))
-  `(ert-deftest
-       ,(intern (replace-regexp-in-string "[ .]" "-" desc)) ()
-     ;; Rebind tables for tests.
-     (let ((ns/symbols-table (copy-hash-table ns/symbols-table)))
-       ,@body)))
-
-
 ;;; Hashing
 
 (check "hash function returns different hashes for different inputs."
--- tests/tests.el
+++ tests/tests.el
@@ -1,8 +1,19 @@
 (provide 'ns-tests '(ns-core-tests
                      ns-operators-tests))
+(require 'ert)
 
 (add-to-list 'load-path (concat user-emacs-directory "elisp-namespaces/" "src/"))
 (add-to-list 'load-path (concat user-emacs-directory "elisp-namespaces/" "tests/"))
+
+(defmacro check (desc &rest body)
+  "Wrap ert-deftest with a simpler interface for testing namespaces."
+  (declare (indent 1))
+  `(ert-deftest
+       ,(intern (replace-regexp-in-string "[ .]" "-" desc)) ()
+     ;; Rebind tables for tests.
+     (let ((ns/symbols-table (copy-hash-table ns/symbols-table)))
+       ,@body)))
+
 
 (require 'ns-core-tests)
 (require 'ns-operators-tests)
