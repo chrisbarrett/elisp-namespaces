9caf25d7d9c590dc9823818573dab5659516219b
Chris Barrett <chris.d.barrett@me.com>
2012-11-03 13:43:45 +1300

    Add tests for @sym

--- tests/ns-operators-tests.el
+++ tests/ns-operators-tests.el
@@ -1,3 +1,14 @@
 (provide 'ns-operators-tests)
 (require 'ns-operators)
+(require 'ns-core)
 (require 'ert)
+
+;;; @sym
+
+(check "@sym returns the hash of the given qualified name"
+  (let ((hash (car (ns/intern 'foo 'bar))))
+    (should (equal hash (@sym foo/bar)))))
+
+(check "@sym returns the hash of the given unqualified name in this namespace"
+  (let ((hash (car (ns/intern 'foo 'bar))))
+    (should (equal hash (@sym bar)))))
