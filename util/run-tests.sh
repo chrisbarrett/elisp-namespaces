398c36bb7c25cd5603c892e4345e391ec82ec1d0
Chris Barrett <chris.d.barrett@me.com>
2013-02-12 01:14:58 +1300

    add travis file

--- util/run-tests.sh
+++ util/run-tests.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+#
+# USAGE:
+#  run-tests.sh [emacs-path]
 
 SRC="./namespaces.el"
 TESTS="./namespaces-tests.el"
 CMD="(ert-run-tests-batch-and-exit)"
 
-emacs -q -l $SRC -l $TESTS --batch --eval ${CMD} || exit 1;
+EMACS=$1
+if [ -z $EMACS ]; then
+    EMACS=emacs
+fi
+
+$EMACS -q -l $SRC -l $TESTS --batch --eval ${CMD} || exit 1;
